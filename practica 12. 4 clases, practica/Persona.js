8d58377341548d18a41b8dcb6c37bc62bfc689d3
Edwin Calle <calleojeda4@gmail.com>
2023-09-28 09:21:03 -0500

    Practica class Persona

--- practica 12. 4 clases, practica/Persona.js
+++ practica 12. 4 clases, practica/Persona.js
@@ -46,7 +46,92 @@ class Persona {
 }
 
 
+// CLASE EMPLEADO...............
 
 
-// let persona1 = new Persona( "Edwin", "Calle", 29);
-// console.log(persona1.toString());
+class Empleado extends Persona{
+    static contadorEmpleados =0;
+constructor( nombre, apellido, edad, sueldo){
+    //llamada del constructor de la clase Persona
+    super(nombre, apellido, edad)
+    this._idEmpleado =++Empleado.contadorEmpleados;
+    this._sueldo=sueldo;
+}
+
+// get IdEmpleadoo
+get IdEmpleadoo(){
+    return this._idEmpleado;
+}
+
+
+// get y set Sueldo
+get Sueldo (){
+    return this._sueldo
+}
+
+set Sueldo (suel){
+this._sueldo =suel;
+}
+
+ toString(){
+
+    return `${super.toString()} ${this._idEmpleado} ${this._sueldo}`;
+   
+ }
+
+}
+
+
+
+// CLASE CLIENTE...................
+class Cliente extends Persona{
+
+    static contadorCliente = 0;
+
+constructor( nombre, apellido, edad, fechaRegistro){
+    //llamada del constructor de la clase Persona
+    super(nombre, apellido, edad);
+    this._idCliente = ++Cliente.contadorCliente;
+    this._fechaRegistro=fechaRegistro;
+}
+// get  Idclienete
+get Idclienete(){
+    return this._idCliente;
+}
+
+// get y set de FechaRegistro
+get FechaRegistro(){
+    return this._fechaRegistro;
+}
+
+set FechaRegistro(fecha){
+this._fechaRegistro =fecha;
+}
+toString(){
+    return `${super.toString()} ${this._idCliente}${this._fechaRegistro}`;
+}
+
+}
+
+
+
+// OBJETOS O INSTANCIA DE LA CLASE PERSONA
+
+let persona1 = new Persona( "Edwin", "Calle", 29);
+console.log(persona1.toString());
+let persona2 = new Persona( "Carlos", "Ramirez", 30);
+console.log(persona2.toString());
+
+// OBJETOS O INSTANCIA DE LA CLASE EMPLEDO
+let empleado1 = new Empleado("Edwin", "Calle", 29, 1360);
+console.log(empleado1.toString());
+let empleado2 = new Empleado("Geovanny", "Calle", 29, 1370);
+console.log(empleado2.toString());
+
+// OBJETOS O INSTANCIA DE LA CLASE Cliente
+let cliente1 = new Cliente("Geovanny", "Calle", 29, new Date());
+console.log(cliente1.toString());
+let cliente2 = new Cliente("Alexandra", "Calle", 28, new Date());
+console.log(cliente2.toString());
+
+
