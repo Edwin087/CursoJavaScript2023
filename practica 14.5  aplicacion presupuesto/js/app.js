34a5718195861e97b6f4fc17ffaef64de561602c
Edwin Calle <calleojeda4@gmail.com>
2023-10-07 11:02:08 -0500

    actuaclización aplicación presupuesto

--- practica 14.5  aplicacion presupuesto/js/app.js
+++ practica 14.5  aplicacion presupuesto/js/app.js
@@ -9,6 +9,8 @@ const egresos = [
 
 let cargarApp = () => {
   cargarCabecero();
+  cargarIngresos();
+  cargarEgresos();
 };
 
 let totalIngresos = () => {
@@ -30,10 +32,84 @@ let totalEgresos = () => {
 let cargarCabecero = () => {
   let presupuesto = totalIngresos() - totalEgresos();
   let porcentajeEgreso = totalEgresos() / totalIngresos();
-  document.getElementById("presupuesto").innerHTML = presupuesto;
-  document.getElementById("porcentaje").innerHTML = porcentajeEgreso;
-  document.getElementById("ingresos").innerHTML = totalIngresos();
-  document.getElementById("egresos").innerHTML = totalEgresos();
+  document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+  document.getElementById("porcentaje").innerHTML =
+    formatoPorcentaje(porcentajeEgreso);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
+//para dar formato moneda
+let formatoMoneda = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
+//formato de porsentaje
+let formatoPorcentaje = (valor) => {
+  return valor.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
+
+const cargarIngresos = () => {
+  let ingresosHTML = "";
+  for (let ingreso of ingresos) {
+    ingresosHTML += crearIngresoHTML(ingreso);
+  }
+  document.getElementById("lista-ingresos").innerHTML = ingresosHTML;
+};
+
+const crearIngresoHTML = (ingresoo) => {
+  let ingresooHTML = `
+                  <div class="elemento limpiarEstilos">
+                    <div class="elemento_descripcion">${
+                      ingresoo.descripcioon
+                    }</div>
+                    <div class="derecha limpiarEstilos">
+                        <div class="elemento_valor">${formatoMoneda(
+                          ingresoo.valoor
+                        )}</div>
+                        <div class="elemento_eliminar">
+                            <button class="elemento_eliminar--btn">
+                                <ion-icon name="close-circle-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+  `;
+  return ingresooHTML;
+};
+
+const cargarEgresos = () => {
+  let egresosHTML = "";
+  for (let egreso of egresos) {
+    egresosHTML += crearEgresosHTML(egreso);
+  }
+  document.getElementById("lista-egresos").innerHTML = egresosHTML;
+};
+
+const crearEgresosHTML = (egresoo) => {
+  let egresosHTML = `
+  <div class="elemento limpiarEstilos">
+<div class="elemento_descripcion"> ${egresoo.descripcioon}</div>
+<div class="derecha limpiarEstilos">
+    <div class="elemento_valor">${formatoMoneda(egresoo.valoor)}</div>
+    <div class="elemento_porcentaje">${formatoPorcentaje(
+      egresoo.valoor / totalEgresos()
+    )}</div>
+    <div class="elemento_eliminar">
+        <button class="elemento_eliminar--btn">
+            <ion-icon name="close-circle-outline"></ion-icon>
+        </button>
+    </div>
+</div>
+</div>`;
+  return egresosHTML;
 };
 
 //window.addEventListener("load", cargarApp, false);
